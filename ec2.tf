62ba40d52162fd266932d0f828579431b1ef4e23
Andrew Waters <andrew.waters@capgemini.com>
2022-01-31 20:06:02 +0000

    update

---
+++ ec2.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "aw-testhost" {
+  instance_type     = "t2.micro"
+  ami           = data.aws_ami.ubuntu.id
+  availability_zone = "eu-west-2a"
+
+  tags = {
+     Name = "cpt-infracost-demo"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
